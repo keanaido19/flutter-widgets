2df60a0527f175733d554a4bc2454f13f8f9f817
keanaido021 <keanaido021@student.wethinkcode.co.za>
2022-10-26 17:27:01 +0200

    Completed Exercise.

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,11 +10,18 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _petrolPricePerLitre = 21;
+  int _petrolPricePerLitre = 0;
+  int _dieselPricePerLitre = 0;
 
   void _incrementPetrolPrice() {
     setState(() {
       _petrolPricePerLitre++;
+    });
+  }
+
+  void _incrementDieselPrice() {
+    setState(() {
+      _dieselPricePerLitre++;
     });
   }
 
@@ -26,32 +33,98 @@ class _MyHomePageState extends State<MyHomePage> {
           icon: const Icon(Icons.settings),
           onPressed: () {},
         ),
-        title: Text(widget.title),
+        title: Center(
+          child: Text(widget.title)
+        ),
         actions: [
           IconButton(
-            icon: const Icon(Icons.add),
-            onPressed: () {},
+            icon: const Icon(Icons.add_circle_rounded),
+            tooltip: 'Increment Diesel Price',
+            onPressed: _incrementDieselPrice,
           )
         ],
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            const Text(
-              'Current Petrol Price:',
+            const Divider(
+              color: Colors.transparent,
             ),
-            Text(
-              'R$_petrolPricePerLitre per litre.',
-              style: Theme
-                  .of(context)
-                  .textTheme
-                  .headline4,
+            Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(24.0),
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(width: 1, color: Colors.green),
+                  borderRadius: const BorderRadius.all(Radius.circular(24)),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                      children: <Widget>[
+                        const Text(
+                          'Current Petrol Price:',
+                        ),
+                        Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            Text(
+                              'R',
+                              style: Theme.of(context).textTheme.headline4,
+                            ),
+                            Text(
+                              '$_petrolPricePerLitre',
+                              style: Theme.of(context).textTheme.headline4,
+                            ),
+                            Text(
+                              ' per litre.',
+                              style: Theme.of(context).textTheme.headline4,
+                            ),
+                          ],
+                        ),
+                      ]
+                  ),
+                ),
+              ),
+            ),
+            const Divider(
+              color: Colors.transparent,
+            ),
+            Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(24.0),
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(width: 1, color: Colors.orangeAccent),
+                  borderRadius: const BorderRadius.all(Radius.circular(24)),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                      children: <Widget>[
+                        const Text(
+                          'Current Diesel Price:',
+                        ),
+                        Text(
+                          'R$_dieselPricePerLitre per litre.',
+                          style: Theme
+                              .of(context)
+                              .textTheme
+                              .headline4,
+                        ),
+                      ]
+                  ),
+                ),
+              ),
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
+        key: const Key("petrolIncrementButton"),
         onPressed: _incrementPetrolPrice,
         tooltip: 'Increment Fuel Price',
         child: const Icon(Icons.add),
